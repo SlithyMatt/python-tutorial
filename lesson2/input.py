4b83b8b1addbc93aa3c31dc122e675c4492fdb88
SlithyMatt <matt@slithygames.com>
2025-03-24 23:06:03 -0400

    first draft of Lesson 2 slides

--- lesson2/input.py
+++ lesson2/input.py
@@ -3,7 +3,7 @@ name = input()
 print("Your name is " + name)
 print("What is your age?")
 age = input()
-if (age.isdigit()):
+if age.isdigit():
    nextage = int(age) + 1
    print("You will be " + str(nextage) + " on your next birthday")
 else:
